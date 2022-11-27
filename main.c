3335500c22f6a0c6642e2e17c4e9ed6da9b4413d
Victor <88071929+victormrtns@users.noreply.github.com>
2022-11-26 21:41:58 -0300

    function done gerar_arquivo, the beggining of calcular_grau function

--- main.c
+++ main.c
@@ -1,5 +1,14 @@
 #include <stdio.h>
 #include <ctype.h>
+#include <malloc.h>
+
+typedef struct Aresta{
+    int vi;
+    int vj;
+    int peso;
+}Aresta;
+
+void calcular_grau();
 FILE* gerar_arquivo();
 int main() {
     FILE *arquivo;
@@ -7,7 +16,41 @@ int main() {
     if(arquivo = fopen("arquivo.txt","r+") == NULL){
           arquivo =gerar_arquivo();
     }
-    else{}
+    else{
+        printf("arquivo.txt existe, deseja criar um novo arquivo? (Sim = 1) ou (Não = 0): \n");
+        scanf("%d",&opcao);
+        while(opcao != 0 || opcao!=1){
+            if(opcao == 0 || opcao ==1){
+                break;
+            }
+            scanf("%d",&opcao);
+        }
+        if(opcao==1){
+            arquivo = gerar_arquivo();
+        }
+    }
+    if(arquivo = fopen("arquivo.txt","r+") == NULL){
+        printf("Erro ao abrir o arquivo");
+        return 0;
+    }
+
+    do{
+        printf("-------MENU-------\n");
+        printf("Digite uma opcao: \n");
+        scanf("%d",&opcao);
+        switch(opcao){
+            case 0:
+                break;
+            case 1:
+                break;
+            case 2:
+                calcular_grau();
+                break;
+        }
+    }while(opcao!=0);
+
+
+
     return 0;
 }
 
@@ -16,6 +59,7 @@ FILE* gerar_arquivo(){
     arquivo = fopen("arquivo.txt","w");
     int num_vertices,num_arestas,valorado;
     char tipo;
+    int i;
     printf("---Gerar Arquivo---\n");
     printf("Numero de Vertices: \n");
     scanf("%d",&num_vertices);
@@ -42,8 +86,57 @@ FILE* gerar_arquivo(){
     }
     printf("O grafo e valorado (nao valorado = 0,valorado=1): \n");
     scanf("%d",&valorado);
-    fprintf(arquivo,"%d %d %c %d",num_vertices,num_arestas,tipo,valorado);
+    switch(valorado){
+        case 0:
+            break;
+        case 1:
+            break;
+        default:
+            do {
+                printf("Insira novamente: \n");
+                scanf("%d", &valorado);
+                if(valorado ==0|| valorado ==1){
+                    break;
+                }
+            }while(valorado !=0 || valorado !=1);
+            break;
+    }
+    fprintf(arquivo,"%d %d %c %d\n",num_vertices,num_arestas,tipo,valorado);
+    Aresta *arestas;
+    int peso = 1;
+    arestas = malloc(num_arestas * sizeof(Aresta));
+    if(valorado ==0){
+        for(i=0;i<num_arestas;i++){
+            printf("Inserir vi: \n");
+            scanf("%d",&arestas[i].vi);
+            printf("Inserir vj: \n");
+            scanf("%d",&arestas[i].vj);
+            arestas[i].peso = peso;
+            fprintf(arquivo,"%d %d %d\n",arestas[i].vi,arestas[i].vj,arestas[i].peso);
+        }
+    }
+    else{
+        for(i=0;i<num_arestas;i++){
+            printf("Inserir vi: \n");
+            scanf("%d",&arestas[i].vi);
+            printf("Inserir vj: \n");
+            scanf("%d",&arestas[i].vj);
+            printf("Inserir peso: \n");
+            scanf("%d",& arestas[i].peso);
+            fprintf(arquivo,"%d %d %d\n",arestas[i].vi,arestas[i].vj,arestas[i].peso);
+        }
+    }
+    fclose(arquivo);
+    return arquivo;
+}
+void calcular_grau() {
+    FILE *arquivo;
+   arquivo =fopen("arquivo.txt","r");
 
+    char tipo;
+    int vertices, arestas, valorado;
+    fscanf(arquivo,"%d %d %c %d",&vertices,&arestas,&tipo,&valorado);
+    fclose(arquivo);
 
 }
 
